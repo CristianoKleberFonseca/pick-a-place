c9df8f134f7baa56cbac4a1c71a7497e54704d16
Cristiano Kleber da Fonseca <cristiano.fonseca@selfitacademias.com.br>
2018-09-26 09:18:18 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/dbserver/pickaplace/PickAPlaceApplication.java
@@ -0,0 +1,12 @@
+package br.com.dbserver.pickaplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickAPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickAPlaceApplication.class, args);
+	}
+}
